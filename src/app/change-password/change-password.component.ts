e8aabc2120157c7f786019495ed716de61c552cd
Dhanavandhan <dhanavandhan@capeindia.net>
2022-12-13 11:09:59 +0530

    Removed unwanted code,
    1. Removed unwanted code which I pushed before in the git.

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -20,26 +20,13 @@ export class ChangePasswordComponent implements OnInit {
   succesMsg: boolean = false;
   changePasswordForm = new FormGroup({
     oldPassword: new FormControl(),
-    newpassword: new FormControl(), fileName: new FormControl(''),
-   
-    fileSize: new FormControl(''),fileid:new FormControl(),fileType:new FormControl(),filename:new FormControl()
-   
+    newpassword: new FormControl(), 
+  
   });
   user = new User();
 
   errorMessage: any;
   showErrorMessage: boolean = false;
-  fileName: String="";
-  fileSize: any;
-  file!: any;
-  formFile:any;
-  fileId:any;
-  shortLink: string = "";
-    loading: boolean = false; // Flag variable
-contentSpinner: any;
-  retreveFileId: any;
-   
-
   constructor(private fb: FormBuilder, private registerService: RegisterserviceService, private route: Router, 
     private globalErrorHandler: GlobalErrorHandlerService,private fileUploadService:FileUploadService
   ) { }
@@ -49,22 +36,10 @@ contentSpinner: any;
     this.changePasswordForm = this.fb.group({
       oldPassword: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)]],
       newpassword: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)]],
-      fileName:[''],
-      fileSize:[''],fileid:[''],filename:[''],fileType:['']
+     
     });
 
-    this.fileUploadService.retriveFile(34).subscribe(data=>{
-      // let temp=[];
-      // temp=JSON.parse(data);
-      // for(let i of temp){
-        this.f.filename.setValue(JSON.parse(data).fileName);
-        this.f.fileType.setValue(JSON.parse(data).fileType);
-        this.f.fileid.setValue(JSON.parse(data).fileId);
-        
-      // }
-      
-      console.log(this.f.filename)
-    })
+   
   }
  
 
@@ -101,53 +76,5 @@ contentSpinner: any;
     return this.changePasswordForm.controls;
   }
 
-  onChange(event:any){
-    this.file = event.target.files;
-    // if (this.file != null) {
-    //   this.uploadDisable = false;
-    // }
-    this.changePasswordForm.controls.fileSize.setValue(Math.round(this.file[0].size / 1024) + " KB");
-    this.fileSize = Math.round(this.file[0].size / 1024) + " KB";
-    this.changePasswordForm.controls.fileSize = this.fileSize;
-    this.fileName = this.file[0].name;
-  }
-  updateFile(){
-    const formData: FormData = new FormData();
-    for (let f of this.file) {
-      formData.append('file', f, f.name);
-    }
-  this.fileId=30;
-    this.formFile = formData;
-    this.fileUploadService.updateFile(formData,this.fileId,this.fileSize).subscribe(data=>{
-          this.ngOnInit();
-    })
-  }
-  onUpload(){
-    const formData: FormData = new FormData();
-    for (let f of this.file) {
-      formData.append('file', f, f.name);
-    }
-    
-    this.formFile = formData;
-            
-   this.fileUploadService.fileUploadLms(formData,this.fileSize).subscribe(data=>{
-    this.retreveFileId=data;
-    this.fileId=parseInt(this.retreveFileId);
-    this.f.fileid.setValue(this.fileId);
-   this.retreveFileId(this.fileId)
-    console.log(this.fileId);
-    
-    console.log("success")
-   },
-   error=>{
-    this.showErrorMessage = true;
-    this.errorMessage = this.globalErrorHandler.errorMessage;
-    setTimeout(() => {
-      this.showErrorMessage = false;
-    }, 3000);
-   })
-  }
-  onDownload(){
-         this.fileUploadService.fileDownload(32);
-  }
+  
 }
