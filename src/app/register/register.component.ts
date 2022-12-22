c4d1becca169c01d19c95b4392dd3d8cb8eb2799
Priyanka <priyanka@capeindia.net>
2022-12-22 16:28:47 +0530

    state issue

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,8 +7,7 @@ import { CscService } from '../csc.service';
 
 
 interface Country {
-  shortName: string;
-  name: string;
+  
 }
 @Component({
   selector: 'app-register',
@@ -39,204 +38,10 @@ export class RegisterComponent implements OnInit {
   Hide:boolean=true;
 
 
-  
-  // subscriptions:Subscription[]=[];
-  
-  // countryDropdownSettings:any=[];
-  // stateDrodownSettings: any;
-  // cityDropdownSettings: any;
-  // countries :ICountry[] = [];
-  // states : IState[] = [];
-  // cities : ICity[] = [];
-  // selectedCountryCode: any;
-  // stateList1: any=[];
-  // countryList: any=[];
- 
- // Only Accept numbers
-  keyPressNumbers(event: any) {
-    var charCode = (event.which) ? event.which : event.keyCode;
-    // Only Numbers 0-9
-    if ((charCode < 48 || charCode > 57)) {
-      event.preventDefault();
-      return false;
-    } else {
-      return true;
-    }
-  }
-  changeCountry1(e: any) {
-     let changedValue;
-     if(e.target != undefined) {
-     changedValue = e.target.value;
-      
-    }
-    else{
-     changedValue = e;
-    }
-    this.stateList1 = [];
-   for(let arr of this.countryList) {
-     if( arr.name == changedValue) {
-   this.siteService.retrieveState(arr.code).subscribe(
-    data => {
-   this.stateList1 = JSON.parse(data) 
-  
-    }
-    )};
-     }
-  }
-// changeCountry1(e:any){
-//   let changedValue;
-//   if(e.target!=undefined){
-//     changedValue = e.target.value;
-
-//   }
-//   else{
-//     changedValue=e;
-//   }
-//   this.countryList=[];
- 
-      
-    
-  
-// }
-  
-//checking empid duplicates 
-  empid(event: any){
-   this.Empid=this.register.empid;
-   
-   let tempArr : any=[];
-   
-   this.registerService.getEmpid().subscribe(
-    data => {
-   
-     tempArr = JSON.parse(data);
-     for(let j of tempArr)
-     {
-      if(j.empid==this.Empid ){
-        this.prod=true;
-      }
-       }
-         setTimeout(() => {
-          this.prod = false;
-          
-        }, 3000);
-        console.log("empid registered")
-        // else{
-        //   console.log("bug")
-        // }
-      }
-    
-     )
-    
-     }
-     // checking mobile number duplicates
- mobile(event:any){
-  // this.register.mobilenumber='+'+ this.countryCode +'-' + this.RegisterationForm.value.mobile
-      this.Mobile= this.register.mobilenumber;
-      let  tempArr : any=[];
-
-this.registerService.getEmpid().subscribe(
-  data =>{
-    tempArr = JSON.parse(data);
-    for(let j of tempArr){
-     
-      if(j.mobilenumber==this.Mobile){
-this.prod1=true;
-setTimeout(() => {
-  this.prod1=false;
-}, 3000);
-console.log("same mobilenumber")
-      }
-    }
-  }
-)
-           
-     }
-     //checking alternate mobile number duplicates
- alter(event:any){
-  // this.register.alternatenumber='+'+ this.countryCode +'-' + this.RegisterationForm.value.alternate
-  this.Alternate = this.register.alternatenumber;
-  let tempArr : any=[];
-  this. registerService.getEmpid().subscribe(
-    data =>{
-      tempArr = JSON.parse(data);
-      for(let j of tempArr){
-        if(j.alternatenumber== this.Alternate){
-          this.prod2=true;
-          setTimeout(()=>{
-            this.prod2=false;
-          }, 3000);
-          console.log("same alternatenumber")
-        }
-      }
-    }
-  )
- }
-
-     
-   //checking email duplicates     
-   email(event:any){
-   
-    this.Email=this.register.emailid;
-    let tempArr:any=[];
-    this.registerService.getEmpid().subscribe(
-      data=>{
-        tempArr = JSON.parse(data);
-        for(let j of tempArr){
-          if(j.emailid==this.Email){
-            this.prod3 = true;
-            setTimeout(()=>{
-              this.prod3=false;
-            },3000);
-            console.log("same email")
-          }
-        }
-      }
-
-    )
-   }
-
-   
-
-  
-
-
-
-  
-  //  getCountries(){
-  //   this.countries=csc.getAllCountries();
-  // }
-  // getState(countryCode:any){
-  //   this.states=csc.getStatesOfCountry(this.countryCode);
-  // }
-
-  // getCity(countryCode:any,stateCode:any){
-  //  this.cities=csc.getCitiesOfState(this.countryCode);
-
-  // }
-  // initDropdownSettings(){
-  //   this.countryDropdownSettings={ 
-  //     singleSelection: true,
-  //     idField: 'isoCode',
-  //     textField: 'name',
-  //      selectAllText: 'Select All',
-  //     unSelectAllText: 'UnSelect All',
-  //     itemsShowLimit :3,
-  //     allowSearchFilter:true,
-  //     maxHeight:'100'
-  //   };
-  // }
- 
-   
-  
   constructor(private formBuilder: FormBuilder,private route: Router,
     private registerService: RegisterserviceService, private siteService:CscService
   ) { 
-    // this.countries = this. siteService.getCountries();
-    // this.form = new FormGroup({
-    //   country: this.country,
-    //   state: this. state,
-    //   city: this.city,
-    // });
+   
   }
   ngOnInit(): void {
     this.RegisterationForm = new FormGroup({
@@ -271,57 +76,146 @@ console.log("same mobilenumber")
     )
 
     this.countryCode='91';
-    // this.initForm();
-    // this.initDropdownSettings();
-    // this.getCountries();
-
-  }
-//   handleValueChanges(){
-//     this.subscriptions.push (this.RegisterationForm.get('country')?.value.subscribe((response: { isoCode: any; }[])=>{
-//       console.log('selected country is', response);
-//       this.selectedCountryCode = response[0].isoCode;
-//       this.getState(response[0].isoCode);
-//       }));
-// this.subscriptions.push(this.RegisterationForm.get('state')?.value.subscribe((response: { isoCode: any; }[]) =>{
-//    console.log('state selection', response);
-//    this.getCity(this.selectedCountryCode,response[0].isoCode);
-// }));
-//     }  
+ }
   
-  initForm(){
-    this.RegisterationForm=this.formBuilder.group({
-      country:[''],
-      state:[''],
-      city:['']
-    })
-   
-  // this.getCountries();
-  // this.initDropdownSettings();
-  // this.countryDropdownSettings();
-  //   this.handleValueChanges();
-   
-
-
-//   this.countryChange.valueChanges.subscribe((country))=>{
-//     this.states.reset();
-//     this.states.disable();
-//     if(country){
-//       this.states=this.siteService.getStatesByCountry(country);
-//       this.states.enable();
-//     }
-//   }
-//  this.states.valueChanges.subscribe((state))=>{
-//   this.city.reset();
-//   this.city.disable();
-//   if(state){
-//     this.cities = this. siteService.getcitiesByState(this.countryChange.value, state);
-//     this.city.enable();
-//   }
-//  }
+  keyPressNumbers(event: any) {
+    var charCode = (event.which) ? event.which : event.keyCode;
+    // Only Numbers 0-9
+    if ((charCode < 48 || charCode > 57)) {
+      event.preventDefault();
+      return false;
+    } else {
+      return true;
+    }
   }
-  getemail(event:any){
+  changeCountry1(e: any) {
+     let changedValue;
+     if(e.target != undefined) {
+     changedValue = e.target.value;
+      
+    }
+    else{
+     changedValue = e;
+    }
+    this.stateList1 = [];
+    this.register.state = '';
+   for(let arr of this.countryList) {
+     if( arr.name == changedValue) {
+   this.siteService.retrieveState(arr.code).subscribe(
+    data => {
+   this.stateList1 = JSON.parse(data) 
+  
+    }
+    )};
+     }
+  }
+
+  
+//checking empid duplicates 
+  empid(event: any){
+   this.Empid=this.register.empid;
+   
+   let tempArr : any=[];
+   
+   this.registerService.getEmpid().subscribe(
+    data => {
+   
+     tempArr = JSON.parse(data);
+     for(let j of tempArr)
+     {
+      if(j.empid==this.Empid ){
+        this.prod=true;
+        this.field.empid.setValue("");
+      }
+       }
+         setTimeout(() => {
+          this.prod = false;
+          
+        }, 3000);
+        console.log("empid registered")
+        
+      }
     
-    if(event.target.value=='gk@capeindia.net'|| event.target.value=='srp@capeindia.net'||event.target.value=='asha@capeindia.net'||event.target.value=='vasanthi@capeindia.net'||event.target.value=='arun@capeindia.net'){
+     )
+    
+     }
+// checking mobile number duplicates
+ mobile(event:any){
+ 
+      this.Mobile= this.register.mobilenumber;
+      let  tempArr : any=[];
+
+this.registerService.getEmpid().subscribe(
+  data =>{
+    tempArr = JSON.parse(data);
+    for(let j of tempArr){
+     
+      if(j.mobilenumber==this.Mobile){
+this.prod1=true;
+this.field.mobile.setValue("");
+setTimeout(() => {
+  this.prod1=false;
+}, 3000);
+console.log("same mobilenumber")
+      }
+    }
+  }
+)
+           
+     }
+     //checking alternate mobile number duplicates
+ alter(event:any){
+ 
+  this.Alternate = this.register.alternatenumber;
+  let tempArr : any=[];
+  this. registerService.getEmpid().subscribe(
+    data =>{
+      tempArr = JSON.parse(data);
+      for(let j of tempArr){
+        if(j.alternatenumber== this.Alternate){
+          this.prod2=true;
+          this.field.alternate.setValue("");
+          setTimeout(()=>{
+            this.prod2=false;
+          }, 3000);
+          console.log("same alternatenumber")
+        }
+      }
+    }
+  )
+ }
+
+ get field(): any {
+  return this.RegisterationForm.controls;
+} 
+   //checking email duplicates     
+   email(event:any){
+   
+    this.Email=this.register.emailid;
+    let tempArr:any=[];
+    this.registerService.getEmpid().subscribe(
+      data=>{
+        tempArr = JSON.parse(data);
+        for(let j of tempArr){
+          if(j.emailid==this.Email){
+            this.prod3 = true;
+            this.field.email.setValue("");
+            setTimeout(()=>{
+              this.prod3=false;
+            },3000);
+            console.log("same email")
+          }
+        }
+      }
+
+    )
+   }
+
+   getemail(event:any){
+    
+    if(event.target.value=='gk@capeindia.net'|| event.target.value=='srp@capeindia.net'||
+    event.target.value=='asha@capeindia.net'||event.target.value=='vasanthi@capeindia.net'||
+    event.target.value=='arun@capeindia.net'){
       this.Hide=false;
     }
     else{
@@ -331,37 +225,6 @@ console.log("same mobilenumber")
 
   }
 
-  
-//   changeCountry(e:any){
-    
-// let changedValue;
-//  if(e.target != undefined) {
-// changedValue = e.target.value;
-//  }
-//  else{
-//  changedValue = e;
-//  }
-// this.stateList1 = [];
-//  for(let arr of this.countryList) {
-// if( arr.name == changedValue) {
-//  this.siteService.retrieveSite(arr.code).subscribe(
-//  data => {
-//  this.stateList1 = JSON.parse(data)
-// }
-//  )};
-
-//   }
-// }
-    
- 
-  get field(): any {
-    return this.RegisterationForm.controls;
-  }
-  
-  
-
-  
-  
   
 changevalue(e:any){
   if(
@@ -425,10 +288,4 @@ clear(){
   Home(){
     this.route.navigate(['/home']);
   }
-
-  
-  // handleButtonClick(){
-  //   if(!this.RegisterationForm.valid) this.RegisterationForm.markAllAsTouched();
-  // }
-  
 }
